8e48b27ec108a328f4afb05ae4a8a14fe3e0bb52
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-12-28 11:44:23 +0200

    Some changes made

---
+++ data_structures.py
@@ -0,0 +1,17 @@
+# Lists
+# names = ["John", "Christian", "Isingizwe", "Others"]
+# print(names[0])
+# Largest number in a list
+
+number_list = [3,2,5,6,7,3,2]
+smallest = number_list[0]
+count = len(number_list)
+
+while count > 0:
+    if number_list[count-1] < smallest:
+        smallest = number_list[count-1]
+count -= 1
+
+print(smallest)
+
+# 2D Lists
