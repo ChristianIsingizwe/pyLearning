ca47283a05283370e551752ab198d731e9009d9d
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-12-28 09:37:41 +0200

    Some changes made

---
+++ strings.py
@@ -0,0 +1,17 @@
+# course = '''
+# Hello everyone what is going on with you.
+# '''
+#
+#
+# courses = 'python tutorial for beginners'
+# print(courses[0:3])
+
+# Formatted strings
+
+first_name = "ISINGIZWE"
+last_name = "Christian"
+
+message = f'{first_name} [{last_name}] is a coder'
+print(message)
+
+print(message.upper())
