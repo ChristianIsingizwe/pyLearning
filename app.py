2727fbfd711c5e3f3bdd6c2df30bde8843a83416
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-12-28 09:20:36 +0200

    Learn about strings

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-print("Hello world")
-print('*' * 10)
+# print("Hello world")
+# print('*' * 10)
 
 
-is_published = False
+birth_year = input("Enter the year you were born in: ")
+print(type(birth_year))
+age = 2024 - int(birth_year)
+print(type(birth_year))
+print(age)
+
+
+# is_published = False
+#
+# name = input("Enter your name: ")
+# print("Hello" + " " + name)
